d6be18fcf574e32742b8faf033b459171217f304
FFWL <51259150+FFWL@users.noreply.github.com>
2021-12-14 21:44:04 +0300

    Writer Page Beginning

--- CoreDemo/Controllers/WriterController.cs
+++ CoreDemo/Controllers/WriterController.cs
@@ -14,5 +14,18 @@ namespace CoreDemo.Controllers
         {
             return View();
         }
+        public IActionResult WriterProfile()
+        {
+            return View();
+        }
+        public IActionResult WriterMail()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Test()
+        {
+            return View();
+        }
     }
 }
